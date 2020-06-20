03c4d8ce1bc65718967f725cf203f844c3c59e59
SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
2020-06-20 15:15:37 +0530

    SDM660: XiaomiParts: Add SELinux Switch
    Reference: https://github.com/DerpLab/platform_packages_apps_DerpQuest/commit/65f48a63a8e43ca041462a9c2280435ad15779a6
    
    Vectors From: https://materialdesignicons.com
    
    @SiddharthBharadwaj edits: Adapt To Parts

--- XiaomiParts/src/org/lineageos/settings/device/BootReceiver.java
+++ XiaomiParts/src/org/lineageos/settings/device/BootReceiver.java
@@ -16,21 +16,90 @@
 
 package org.lineageos.settings.device;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
+import android.content.SharedPreferences;
+import android.os.SELinux;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.lineageos.settings.device.R;
 
 import org.lineageos.settings.device.kcal.Utils;
 
 import java.lang.Math.*;
 
+import java.io.IOException;
+import java.util.List;
+
 public class BootReceiver extends BroadcastReceiver implements Utils {
+
+    private static final String PREF_SELINUX_MODE = "selinux_mode";
+
+    private Context settingsContext = null;
+    private static final String TAG = "SettingsOnBoot";
+    private boolean mSetupRunning = false;
+    private Context mContext;
 
     public static final  String HEADPHONE_GAIN_PATH = "/sys/kernel/sound_control/headphone_gain";
     public static final  String MIC_GAIN_PATH = "/sys/kernel/sound_control/mic_gain";
 
     public void onReceive(Context context, Intent intent) {
+
+    mContext = context;
+    ActivityManager activityManager =
+            (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+    List<ActivityManager.RunningAppProcessInfo> procInfos =
+            activityManager.getRunningAppProcesses();
+    for(int i = 0; i < procInfos.size(); i++) {
+        if(procInfos.get(i).processName.equals("com.google.android.setupwizard")) {
+            mSetupRunning = true;
+        }
+    }
+
+    if(!mSetupRunning) {
+        try {
+            settingsContext = context.createPackageContext("com.android.settings", 0);
+        } catch (Exception e) {
+            Log.e(TAG, "Package not found", e);
+        }
+        SharedPreferences sharedpreferences = context.getSharedPreferences("selinux_pref",
+                Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(PREF_SELINUX_MODE)) {
+            boolean currentIsSelinuxEnforcing = SELinux.isSELinuxEnforced();
+            boolean isSelinuxEnforcing =
+                    sharedpreferences.getBoolean(PREF_SELINUX_MODE,
+                            currentIsSelinuxEnforcing);
+            if (isSelinuxEnforcing) {
+                if (!currentIsSelinuxEnforcing) {
+                    try {
+                        SuShell.runWithSuCheck("setenforce 1");
+                        showToast(context.getString(R.string.selinux_enforcing_toast_title),
+                                context);
+                    } catch (SuShell.SuDeniedException e) {
+                        showToast(context.getString(R.string.cannot_get_su), context);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                if (currentIsSelinuxEnforcing) {
+                    try {
+                        SuShell.runWithSuCheck("setenforce 0");
+                        showToast(context.getString(R.string.selinux_permissive_toast_title),
+                                context);
+                    } catch (SuShell.SuDeniedException e) {
+                        showToast(context.getString(R.string.cannot_get_su), context);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
         if (Settings.Secure.getInt(context.getContentResolver(), PREF_ENABLED, 0) == 1) {
             FileUtils.setValue(KCAL_ENABLE, Settings.Secure.getInt(context.getContentResolver(),
@@ -75,4 +144,9 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
         FileUtils.setProp(DeviceSettings.HALL_WAKEUP_PROP, Settings.Secure.getInt(
                 context.getContentResolver(), DeviceSettings.PREF_HALL_WAKEUP, 1) == 1);
     }
+    private void showToast(String toastString, Context context) {
+    Toast.makeText(context, toastString, Toast.LENGTH_SHORT)
+            .show();
 }
+}
+
